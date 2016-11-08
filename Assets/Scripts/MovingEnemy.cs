95248c30a3d6f0d337af7783a13220d0f626b03e
Nicolai Berg <Nicolai Berg>
2016-11-08 16:45:46 +0100

    Targetscripts

---
+++ Assets/Scripts/MovingEnemy.cs
@@ -0,0 +1,47 @@
+﻿﻿using UnityEngine;
+using Zenject;
+
+public class MovingEnemy : Enemy
+{
+    public float speed = 25;
+    private Vector3 _path;
+    public bool OnTheMove;
+    public GameObject DestinationPath;
+    private Vector3 _startPosition;
+    
+    void Start()
+    {
+        _startPosition = gameObject.transform.position;
+        _path = DestinationPath.transform.position;
+    }
+
+    void Update()
+    {
+        base.Update();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float step = speed * Time.deltaTime;
+        if (Alive)
+        {
+            if (!OnTheMove)
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, _startPosition, step);
+                if (gameObject.transform.position == _startPosition)
+                {
+                    OnTheMove = true;
+                }
+            }
+            else
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, _path, step);
+                if (gameObject.transform.position == _path)
+                {
+                    OnTheMove = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ShootbackEnemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class ShootbackEnemy : Enemy
+{
+    private int TargetLives = 5;
+    public float Timer;
+    public GameObject Projectile;
+    public AudioSource TargetLock;
+
+    void Start()
+    {
+        LockOnTargetSound();
+    }
+
+    void Update()
+    {
+        base.Update();
+        TargetPlayer();
+    }
+
+    private void LockOnTargetSound()
+    {
+        TargetLock.Play();
+    }
+
+    private void TargetPlayer()
+    {
+        Timer += Time.deltaTime;
+        if(Timer >= 5f)
+        {
+            ShootProjectile();
+            Timer = 0;
+            LockOnTargetSound();
+        }
+    }
+    private void ShootProjectile()
+    {
+        Instantiate(Projectile, gameObject.transform);
+    }
+}
